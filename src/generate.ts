1588fefe16f0cf7e62298fd5150581dbca73e413
Phạm Ngọc Quang Nam <NamPNQ@users.noreply.github.com>
2021-08-30 18:35:05 +0200

    Correct fallbackEntry

--- src/generate.ts
+++ src/generate.ts
@@ -130,7 +130,7 @@ class Generator {
     const fallbackEntry = mediaTypeEntries[0]
 
     if (fallbackEntry) {
-      return { description, required, mediaType: textEntry[0], type: "any" }
+      return { description, required, mediaType: fallbackEntry[0], type: "any" }
     }
 
     return null
